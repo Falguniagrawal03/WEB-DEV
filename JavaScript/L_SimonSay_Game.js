bbba6d0e62e0307aa7e3f698d2a7955365b7296a
Falguni Agrawal <falguniagrawal1703@gmail.com>
2026-07-31 22:30:38 +0530

    Update L_SimonSay_Game.js

--- JavaScript/L_SimonSay_Game.js
+++ JavaScript/L_SimonSay_Game.js
@@ -1,12 +1,39 @@
 let gameSeq=[];
 let userSeq=[];
 
+let btns = ["pink", "green", "blue", "purple"];
+
 let started = false;
 let level = 0;
+
+let h2 = document.querySelector("h2");
 
 document.addEventListener("keypress", function(){
     if(started==false){
         console.log("game is started");
-        started=true;
+        started = true;
+
+        levelUp();
     }
 });
+
+function btnFlash(btn){
+    btn.classList.add('flash');
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250);
+}
+
+function levelUp(){
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    //random btn choose
+    let randIdx = Math.floor(Math.random() * 3);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    console.log(randIdx);
+    console.log(randColor);
+    console.log(randBtn);
+    btnFlash(randBtn);
+}
